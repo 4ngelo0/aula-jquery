376dc79f51841e5a953b9a1e724bddfbe1c6602a
4ngelo0 <gabrielb4rr00s@gmail.com>
2022-08-03 09:20:47 -0300

    preventDefault

--- js/manipulation.js
+++ js/manipulation.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
     // AS manipulaçoes que eu desejar devem estar dentro deste bloco
-    $('#btn-1-5em').click(function(){
+    $('#btn-1-5em').click(function(e){
+        e.preventDefault(); //n define a função do html e sim a função de minha escolha
     $('p').removeClass('font-2-0');
     $('p').removeClass('font-2-5');
     $('p').addClass('font-1-5');
     });
-    $('#btn-2-0em').click(function(){
+
+    $('#btn-2-0em').click(function(e){
+        e.preventDefault()
     $('p').removeClass('font-1-0');
     $('p').removeClass('font-2-5');
     $('p').addClass('font-2-0');
     });
-    $('#btn-2-5em').click(function(){
+
+    $('#btn-2-5em').click(function(e){
+        e.preventDefault()
     $('p').removeClass('font-1-0');
     $('p').removeClass('font-2-0');
     $('p').addClass('font-2-5');
